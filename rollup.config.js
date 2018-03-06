03230921bb7d18a1e7e3e0e25e0d35d2f3253796
Pei Tang <pei.tang@zd-automotive.de>
2018-03-06 14:10:12 +0100

    fix rollup config for commonJS(nodejs)

--- rollup.config.js
+++ rollup.config.js
@@ -12,7 +12,23 @@ export default [
       {
         file: pkgInfo.main,
         format: 'cjs'
-      },
+      }
+    ],
+    external: ['bufferutil', 'utf-8-validate', 'tty', 'util', 'fs', 'net', 'url', 'child_process', 'http', 'https', 'events', 'crypto', 'buffer', 'zlib', 'assert', 'stream', 'os'],
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      json(),
+      commonjs(),
+      babel({
+        exclude: ['node_modules/**']
+      })
+    ]
+  },
+  {
+    input: 'index.js',
+    output: [
       {
         file: pkgInfo.module,
         format: 'es'
