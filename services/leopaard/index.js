b18e4140a7282eefd68862f51b42bad786132635
Jiang Bian <jiang.bian@zd-automotive.de>
2018-03-20 19:31:02 +0100

    added tbox-simulator and tsp-simulator SDK interface

--- services/leopaard/index.js
+++ services/leopaard/index.js
@@ -1,7 +1,65 @@
 const Event = require('events');
 const axios = require("axios");
 
-module.exports = class Leopaard {
+class Executor {
+    constructor(option) {
+        this.emitter = new Event();
+        this.option = option;
+    }
+
+    listen() {
+        return this.emitter;
+    }
+
+    run(script, cb) {
+        axios
+            .post(`http://${this.option.host}:${this.option.port}/leopaard/executor/run`, {
+                script
+            })
+            .then(res => {
+                cb(false, res.data);
+            })
+            .catch(err => {
+                cb(true, err);
+            });
+    }
+}
+
+class TboxSimulator {
+    constructor(option) {
+        this.emitter = new Event();
+        this.option = option;
+    }
+
+    listen() {
+        return this.emitter;
+    }
+
+    connect(option, cb) {
+        axios
+            .post(`http://${this.option.host}:${this.option.port}/leopaard/tbox-simulator/connect`, {
+                hostname: option.hostname, port: option.port
+            })
+            .then(res => {
+                cb(false, res.data);
+            })
+            .catch(err => {
+                cb(true, err);
+            });
+    }
+    disconnect(cb) {
+        axios
+            .get(`http://${this.option.host}:${this.option.port}/leopaard/tbox-simulator/disconnect`)
+            .then(res => {
+                cb(false, res.data);
+            })
+            .catch(err => {
+                cb(true, err);
+            });
+    }    
+}
+
+class TspSimulator {
     constructor(option) {
         this.emitter = new Event();
         this.option = option;
@@ -23,5 +81,10 @@ module.exports = class Leopaard {
                 cb(true, err);
             });
     }
+}
 
+module.exports = {
+    Executor: Executor,
+    TboxSimulator: TboxSimulator,
+    TspSimulator: TspSimulator,
 }
--- test/services/leopaard/test.js
+++ test/services/leopaard/test.js
@@ -2,11 +2,20 @@ const fs = require('fs')
 const path = require('fs')
 const Leopaard = require('../../../services/leopaard')
 
-const executor = new Leopaard({ host: 'localhost', port: 9008 })
+const executor = new Leopaard.Executor({ host: 'localhost', port: 9008 })
+const tboxSimulator = new Leopaard.TboxSimulator({ host: 'localhost', port: 9008 })
 
 const script = fs.readFileSync('./tbox-fsm.js')
-console.log('script', script.toString('utf8'))
+//console.log('script', script.toString('utf8'))
 
-executor.run(script.toString('utf8'), (result) => {
+executor.run(script.toString('utf8'), (failed, result) => {
     console.log(result)
+})
+
+tboxSimulator.connect({ hostname: 'localhost', port: 8888 }, (failed, result) => {
+    console.log('connected!')
+})
+
+setTimeout(tboxSimulator.disconnect.bind(tboxSimulator), 1000, (flag)=>{
+    console.log('disconnected!')
 })
