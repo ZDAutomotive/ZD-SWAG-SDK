f14d69521d45d37f1188c9f23a14a70ae7410fe1
zdyujiezhou <yujie.zhou@zd-automotive.de>
2018-03-20 09:59:24 +0100

    added power-switch socket

---
+++ services/power-switch/index.js
@@ -0,0 +1,63 @@
+import ioClient from 'socket.io-client';
+import axios from 'axios';
+
+export default class POWERSwitch {
+  constructor(option) {
+    this.port = option.port || 6007;
+    this.host = option.host || 'localhost'
+    this.subscribeMap = {}
+  }
+
+  connect(type) {
+    return new Promise((resolve, reject) => {
+      this.socket = ioClient.connect(`http://${this.host}:${this.port}/`);
+      this.socket.on('connect', () => {
+        resolve(1)
+        this.socket.emit('identity', type)
+        this.socket.removeAllListeners('connect')
+        this.socket.removeAllListeners('connect_error')
+      })
+      this.socket.on('connect_error', () => {
+        reject(1)
+        this.socket.removeAllListeners('connect')
+        this.socket.removeAllListeners('connect_error')
+        delete this.socket
+      })
+    })
+  }
+
+  /**
+   * poweron
+   */
+  async powerOn() {
+    if (!this.socket) throw new Error('Power Switch service not ready')
+    await axios.post(`http://${this.host}:${this.port}/poweron`)
+    return true;
+  }
+
+  /**
+   * poweroff
+   */
+  async powerOff() {
+    if (!this.socket) throw new Error('Power Switch service not ready')
+    await axios.post(`http://${this.host}:${this.port}/poweroff`)
+
+    return true
+  }
+  /**
+   * selectdevice
+   */
+  async selectDevice(addr) {
+    if (!this.socket) throw new Error('Power Switch service not ready')
+    await axios.post(`http://${this.host}:${this.port}/selectdevice`, {addr});
+    return true
+  }
+  /**
+   * readcurrent
+   */
+  async readCurrent(){
+    if(!this.socket) throw new Error('Current monitoring service not ready')
+    let res = await axios.get(`http://${this.host}:${this.port}/readcurrent`)
+    return res.data
+  }
+}
