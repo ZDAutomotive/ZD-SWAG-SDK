56ee96cc4f1ea5c7ed8bae01481a95da927fc3d2
Jiang Bian <jiang.bian@zd-automotive.de>
2018-03-27 14:06:18 +0000

    finished first draft of Leopaard Demo

--- test/services/leopaard/test.js
+++ test/services/leopaard/test.js
@@ -25,6 +25,16 @@ let unlockMessage = {
         }
 }
 
+function setTimestamp(timestamp) {
+    let d = new Date()
+    timestamp[0] = Number(d.getFullYear().toString().slice(-2))
+    timestamp[1] = d.getMonth()+1
+    timestamp[2] = d.getDate()
+    timestamp[3] = d.getHours()
+    timestamp[4] = d.getMinutes()
+    timestamp[5] = d.getSeconds()
+}
+
 tspSimulator.start({ hostname: 'localhost', port: 8888 }, (failed, result) => {
     if (!failed) {
         console.log('TSP Simulator started!')
@@ -36,6 +46,8 @@ tspSimulator.start({ hostname: 'localhost', port: 8888 }, (failed, result) => {
 tboxSimulator.connect({ hostname: 'localhost', port: 8888 }, (failed, result) => {
     if (!failed) {
         console.log('connected!')
+        
+        setTimestamp(unlockMessage.data.timestamp)
         tspSimulator.send(unlockMessage, (failed, result) => {
             if (!failed) {
                 console.log(result)
@@ -55,27 +67,11 @@ setTimeout(tspSimulator.stop.bind(tspSimulator), 2500, (failed, result) => {
     console.log('tspSimulator.stop() called!')
 })
 
+// send response message
 tboxSimulator.listen().on('message', message => {
-    console.log('TBOX:', message)
-
-    message.dataLength += 2
-
-    let d = new Date()
-    message.data.timestamp[0] = d.getFullYear()-2000
-    message.data.timestamp[1] = d.getMonth()
-    message.data.timestamp[2] = d.getDay()
-    message.data.timestamp[3] = d.getHours()
-    message.data.timestamp[4] = d.getMinutes()
-    message.data.timestamp[5] = d.getSeconds()
-
-    message.response = 1
-    message.data.result = 0
-
-    tboxSimulator.send(message,(result)=>{
-        console.log(result)
-    })
+    console.log('TBOX RECV:', message)
 })
 
 tspSimulator.listen().on('message', message => {
-    console.log('TSP:', message)
+    console.log('TSP RECV:', message)
 })
