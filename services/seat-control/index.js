5f6adeed2c67220a6f37f1e0b9adc63588860150
yuxiangchen <yuxiang.chen@zd-automotive.de>
2018-09-03 21:08:03 +0800

    add set range api for seat control

--- services/seat-control/index.js
+++ services/seat-control/index.js
@@ -43,6 +43,25 @@ export default class seatControl {
   }
 
   /**
+   * set seat position range 
+   * value range 
+    slv- de(32297-32837) cn(32239-32768) cn2 (32262-32802)
+    lnv- de(32331-33467) cn(32068-33209) cn2 (32288-33346)
+    shv- de(32635-33132) cn(32569-33087) cn2 (32359-32845)
+    snv- de(32607-32878) cn(32547-32811) cn2 (32599-32859)
+    minimum = [32262,32288,32359,32599],
+    maximum = [32802,33346,32845,32859]
+   */
+  async setRange(minimum,maximum) {
+    if (!this.socket) throw new Error('Service not ready')
+    let res = await axios.post(`http://${this.host}:${this.port}/setrange`, {
+      min: minimum,
+      max: maximum
+    })
+    return res.data;
+  }
+
+  /**
    * reset the seat position
    */
   async resetPosition() {
--- test/services/seatcontrol/testRange.js
+++ test/services/seatcontrol/testRange.js
@@ -6,7 +6,7 @@ const SC = new swag.SeatControl({host:'192.168.178.114'});
       let conn = await SC.connect();
       console.log(conn);
 //      console.log(SC.socket);
-      let res = await SC.setPosition([32440,33208,32261,32600])
+      let res = await SC.setRange([32401,32196,32255,32539],[32696,33231,32754,32810]);
       console.log(res);
 //      let res2 = await SC.resetPosition()
 //      console.log(res2);
