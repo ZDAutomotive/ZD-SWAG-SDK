5640d797afe13f581c9c9b7f4ca5185c1daf5d1d
gaolibin <libin.gao@zd-automotive.cn>
2020-05-18 10:31:48 +0800

    Testcase config file API

---
+++ test/services/testcaseconfig/test.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const swag = require('../../../dist/bundle.cjs');
+const TCC = new swag.TestcaseConfig({host:'192.168.1.6'});
+
+(async () => {
+    try {
+      let conn = await TCC.connect();
+      console.log(conn);
+
+      let res = await TCC.getCfg()
+      console.log(res.data);
+
+      let res1 = await TCC.setCfg({test:'test111'})
+      console.log(res1)
+
+      let res2 = await TCC.downloadCfg()
+      console.log(res2.headers['content-disposition'].filename)
+
+      let res3 = await TCC.uploadCfg(fs.createReadStream('/home/glb/Code/TCconfig/testcasecfg.json'))
+      console.log(res3)
+    } catch (error) {
+      console.log(error)
+    }
+  })();
